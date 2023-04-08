810857e9b06041567c37417c02de4c2b53beb84b
arman <arman.u.alam@gmail.com>
2023-04-08 13:58:52 +0600

    chore : modified Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:15
 WORKDIR /app
 COPY package.json .
 RUN npm install
-COPY . .
-EXPOSE 3000
-CMD [ "node", "index.js" ]
+COPY . ./
+ENV PORT 3000
+EXPOSE $PORT
+CMD [ "npm", "run", "dev" ]
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('<h2>ddsfdsff</h2>');
 })
 
 const port = process.env.PORT || 3000;
