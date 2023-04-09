c5c3b0725b09b18ead0d38b948cb658d165f4b37
arman <arman.u.alam@gmail.com>
2023-04-09 12:07:29 +0600

    chore : added env in yml file

--- index.js
+++ index.js
@@ -1,6 +1,37 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const { MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT } = require('./config/config');
 
 const app = express();
+
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+    mongoose.connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+        .then(() => console.log('successfully connected'))
+        .catch(e => {
+            console.log(e);
+            setTimeout(connectWithRetry, 5000);
+        });
+}
+
+connectWithRetry();
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.get('/', (req, res) => {
     res.send('<h2>dfsdfs</h2>');
